a957c534545ebfd71041389e340259d79cec4740
Xiang Gong <xgong@ece.neu.edu>
2016-10-26 12:44:25 -0400

    * Updated trace parser

--- modules/trace/parser.go
+++ modules/trace/parser.go
@@ -15,17 +15,58 @@ func parseNamedGroup(pattern, str string) map[string]string {
 	return result
 }
 
-// ParseClock example: 'c clk=1000'
+// ParseClock eg: 'c clk=1000'
 func ParseClock(raw string) map[string]string {
 	return parseNamedGroup(`c clk=(?P<clock>\d+)`, raw)
 }
 
-// ParseInstructionUniqueID example: 'id=69 cu=0'
+// ParseInstructionUniqueID eg: 'id=69 cu=0'
 func ParseInstructionUniqueID(raw string) map[string]string {
 	return parseNamedGroup(`id=(?P<id>\d+) cu=(?P<cu>\d+)`, raw)
 }
 
-// ParseInstructionNew example: 'si.new_inst id=69 cu=0 ib=0 wg=0 wf=5 uop_id=8 stg="f" asm="s_load_dwordx4 s[8:11], s[2:3], 0x60 // 0000022C: C0880358"'
+// ParseInstructionNew eg: 'si.new_inst id=69 cu=0 ib=0 wg=0 wf=5 uop_id=8
+// stg="f" asm="s_load_dwordx4 s[8:11], s[2:3], 0x60 // 0000022C: C0880358"'
 func ParseInstructionNew(raw string) map[string]string {
-	return parseNamedGroup(`si.new_inst id=(?P<id>\d+) cu=(?P<cu>\d+) ib=(?P<ib>\d+) wg=(?P<wg>\d+) wf=(?P<wf>\d+) uop_id=(?P<uop_id>\d+) stg="(?P<stage>.+)" asm="(?P<asm>.*)"`, raw)
+	return parseNamedGroup(`si.new_inst id=(?P<id>\d+) cu=(?P<cu>\d+) ib=(?P<ib>\d+) wg=(?P<wg>\d+) wf=(?P<wf>\d+) uop_id=(?P<uop_id>\d+) stg="(?P<stg>.+)" asm="(?P<asm>.*)"`, raw)
+}
+
+// ParseInstructionExecute eg: 'si.inst id=60 cu=0 wf=4 uop_id=7 stg="su-r"'
+func ParseInstructionExecute(raw string) map[string]string {
+	return parseNamedGroup(`si.inst id=(?P<id>\d+) cu=(?P<cu>\d+) wf=(?P<wf>\d+) uop_id=(?P<uop_id>\d+) stg="(?P<stg>.+)"`, raw)
+}
+
+// ParseInstructionEnd eg: 'si.end_inst id=35 cu=3'
+func ParseInstructionEnd(raw string) map[string]string {
+	return parseNamedGroup(`si.end_inst id=(?P<id>\d+) cu=(?P<cu>\d+)`, raw)
+}
+
+// ParseMemoryUniqueID eg: 'name="A-227"'
+func ParseMemoryUniqueID(raw string) map[string]string {
+	return parseNamedGroup(`name="A-(?P<id>\d+)"`, raw)
+}
+
+// ParseMemoryNewAccess eg: 'mem.new_access name="A-227" type="load" state="l1-cu02:load" addr=0xc610'
+func ParseMemoryNewAccess(raw string) map[string]string {
+	return parseNamedGroup(`mem.new_access name="A-(?P<id>\d+)" type="(?P<type>\w+)" state="(?P<module>[^\"\:]+):(?P<action>[^\"\:]+)" addr=(?P<addr>\w+)`, raw)
+}
+
+// ParseMemoryAccess eg: mem.access name="A-213" state="l1-cu0:find_and_lock"
+func ParseMemoryAccess(raw string) map[string]string {
+	return parseNamedGroup(`mem.access name="A-(?P<id>\d+)" state="(?P<module>[^\"\:]+):(?P<action>\w+)"`, raw)
+}
+
+// ParseMemoryEndAccess eg: mem.end_access name="A-16512"
+func ParseMemoryEndAccess(raw string) map[string]string {
+	return parseNamedGroup(`mem.end_access name="A-(?P<id>\d+)"`, raw)
+}
+
+// ParseMemoryNewAccessBlock eg: mem.new_access_block cache="l2-4" access="A-64385" set=37 way=14
+func ParseMemoryNewAccessBlock(raw string) map[string]string {
+	return parseNamedGroup(`mem.new_access_block cache="(?P<cache>(?P<level>\w+)-(?P<module>\w+))" access="(?P<id>A-\d+)" set=(?P<set>\d+) way=(?P<way>\d+)`, raw)
+}
+
+// ParseMemoryEndAccessBlock eg: mem.end_access_block cache="l2-3" access="A-16705" set=101 way=15
+func ParseMemoryEndAccessBlock(raw string) map[string]string {
+	return parseNamedGroup(`mem.end_access_block cache="(?P<cache>(?P<level>\w+)-(?P<module>\w+))" access="(?P<id>A-\d+)" set=(?P<set>\d+) way=(?P<way>\d+)`, raw)
 }
--- modules/trace/parser_test.go
+++ modules/trace/parser_test.go
@@ -2,50 +2,104 @@ package trace
 
 import "testing"
 
+func testField(t *testing.T, result map[string]string, name, expect string) {
+	if result[name] != expect {
+		t.Errorf("RETURN = %s, EXPECT %s", result[name], expect)
+	}
+}
+
 func TestParseClock(t *testing.T) {
 	testString := "c clk=1000"
 	result := ParseClock(testString)
-	if result["clock"] == "1000" {
-		return
-	}
-	t.Error("ParseClock failed")
+	testField(t, result, "clock", "1000")
 }
 
 func TestParseInstructionUniqueID(t *testing.T) {
-	testString := "id=69 cu=0"
+	testString := "id=1 cu=2"
 	result := ParseInstructionUniqueID(testString)
-	if result["id"] == "69" && result["cu"] == "0" {
-		return
-	}
-	t.Error("ParseInstructionUniqueID failed")
+	testField(t, result, "id", "1")
+	testField(t, result, "cu", "2")
 }
 
 func TestParseInstructionNew(t *testing.T) {
 	testString := `si.new_inst id=1 cu=2 ib=3 wg=4 wf=5 uop_id=6 stg="f" asm="s_load_dwordx4 s[8:11], s[2:3], 0x60 // 0000022C: C0880358"`
 	result := ParseInstructionNew(testString)
-	if result["id"] != "1" {
-		t.Errorf("RETURN = %s, EXPECT 1", result["id"])
-	}
-	if result["cu"] != "2" {
-		t.Errorf("RETURN = %s, EXPECT 2", result["cu"])
-	}
-	if result["ib"] != "3" {
-		t.Errorf("RETURN = %s, EXPECT 3", result["ib"])
-	}
-	if result["wg"] != "4" {
-		t.Errorf("RETURN = %s, EXPECT 4", result["wg"])
-	}
-	if result["wf"] != "5" {
-		t.Errorf("RETURN = %s, EXPECT 5", result["wf"])
-	}
-	if result["uop_id"] != "6" {
-		t.Errorf("RETURN = %s, EXPECT 6", result["uop_id"])
-	}
-	if result["stage"] != "f" {
-		t.Errorf("RETURN = %s, EXPECT f", result["stage"])
-	}
-	if result["asm"] != "s_load_dwordx4 s[8:11], s[2:3], 0x60 // 0000022C: C0880358" {
-		t.Errorf("RETURN = %s, EXPECT sload_dwordx4 s[8:11], s[2:3], 0x60 // 0000022C: C0880358|", result["asm"])
-	}
-	return
+	testField(t, result, "id", "1")
+	testField(t, result, "cu", "2")
+	testField(t, result, "ib", "3")
+	testField(t, result, "wg", "4")
+	testField(t, result, "wf", "5")
+	testField(t, result, "uop_id", "6")
+	testField(t, result, "stg", "f")
+	testField(t, result, "asm", "s_load_dwordx4 s[8:11], s[2:3], 0x60 // 0000022C: C0880358")
+
+}
+
+func TestParseInstructionExecute(t *testing.T) {
+	testString := `si.inst id=1 cu=2 wf=3 uop_id=4 stg="su-r"`
+	result := ParseInstructionExecute(testString)
+	testField(t, result, "id", "1")
+	testField(t, result, "cu", "2")
+	testField(t, result, "wf", "3")
+	testField(t, result, "uop_id", "4")
+	testField(t, result, "stg", "su-r")
+}
+
+func TestParseInstructionEnd(t *testing.T) {
+	testString := `si.end_inst id=1 cu=2`
+	result := ParseInstructionEnd(testString)
+	testField(t, result, "id", "1")
+	testField(t, result, "cu", "2")
+}
+
+func TestParseMemoryUniqueID(t *testing.T) {
+	testString := `name="A-1000"`
+	result := ParseMemoryUniqueID(testString)
+	testField(t, result, "id", "1000")
+}
+
+func TestParseMemoryNewAccess(t *testing.T) {
+	testString := `mem.new_access name="A-1000" type="load" state="l1-cu02:load" addr=0xc610`
+	result := ParseMemoryNewAccess(testString)
+	testField(t, result, "id", "1000")
+	testField(t, result, "type", "load")
+	testField(t, result, "module", "l1-cu02")
+	testField(t, result, "action", "load")
+	testField(t, result, "addr", "0xc610")
+}
+
+func TestParseMemoryAccess(t *testing.T) {
+	testString := `mem.access name="A-123" state="l1-cu0:find_and_lock"`
+	result := ParseMemoryAccess(testString)
+	testField(t, result, "id", "123")
+	testField(t, result, "module", "l1-cu0")
+	testField(t, result, "action", "find_and_lock")
+}
+
+func TestParseMemoryEndAccess(t *testing.T) {
+	testString := `mem.end_access name="A-1000"`
+	result := ParseMemoryEndAccess(testString)
+	testField(t, result, "id", "1000")
+}
+
+func TestParseMemoryNewAccessBlock(t *testing.T) {
+	testString := `mem.new_access_block cache="l2-4" access="A-64385" set=37 way=14`
+	result := ParseMemoryNewAccessBlock(testString)
+	testField(t, result, "cache", "l2-4")
+	testField(t, result, "level", "l2")
+	testField(t, result, "module", "4")
+	testField(t, result, "id", "A-64385")
+	testField(t, result, "set", "37")
+	testField(t, result, "way", "14")
+}
+
+func TestParseMemoryEndAccessBlock(t *testing.T) {
+	testString := `mem.end_access_block cache="l2-3" access="A-16705" set=101 way=15`
+	result := ParseMemoryEndAccessBlock(testString)
+	testField(t, result, "cache", "l2-3")
+	testField(t, result, "level", "l2")
+	testField(t, result, "module", "3")
+	testField(t, result, "id", "A-16705")
+	testField(t, result, "set", "101")
+	testField(t, result, "way", "15")
 }
